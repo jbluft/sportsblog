e34bf2114d15d37445d1b9d1420c4dff0369100f
Ketaki Thatte <kthatte@drf.corp>
2018-04-26 15:12:33 -0400

    added registration form

--- client/src/pages/Users/register.js
+++ client/src/pages/Users/register.js
@@ -6,19 +6,58 @@ import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
 import { Input, TextArea, FormBtn } from "../../components/Form";
+import Books from "../Books/Books";
 
 
 class Users extends Component {
+    state = {
+
+        username: "",
+        password: ""
+    };
+    handleFormSubmit = event => {
+        event.preventDefault();
+        if (this.state.username && this.state.password) {
+            API.saveUser({
+                username: this.state.username,
+                author: this.state.author,
+                synopsis: this.state.synopsis
+            })
+                .then(res => this.loadBooks())
+                .catch(err => console.log(err));
+        }
+    };
     render() {
         return (
             <Container fluid>
-            <Row>
+        <Row>
         <Col size="md-6">
             <h1>Register</h1>
-            </Col>
-        </Row>
+        <form>
+        <Input
+        value={this.state.username}
+        onChange={this.handleInputChange}
+        name="username"
+        placeholder="Username (required)"
+            />
+            <Input
+        value={this.state.password}
+        onChange={this.handleInputChange}
+        name="password"
+        placeholder="Password (required)"
+            />
 
-            </Container>
+            <FormBtn
+        disabled={!(this.state.username && this.state.password)}
+        onClick={this.handleFormSubmit}
+    >
+        Submit User
+        </FormBtn>
+        </form>
+        </Col>
+
+        </Row>
+        </Container>
 
         );
     }
